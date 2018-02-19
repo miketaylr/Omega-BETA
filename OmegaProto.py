5429790f2d226dba43a8662a8af2e4916cdf22f2
yuHyuHyuHmaster <36549107+yuHyuHyuHmaster@users.noreply.github.com>
2018-02-19 17:40:27 -0600

    just adding fam

--- OmegaProto.py
+++ OmegaProto.py
@@ -1,10 +1,15 @@
-allCharacters = ["Omega"]
+allCharacters = ['Omega, Nebula']
 print("current characters are:")
-print (allCharacters)
+print allCharacters
 P1_character1 = input("whats is your first character going to be?")
-if P1_character1 = ("Omega"):
-    P1_character1_1move1 = "Dark Slash"
-    P1_character1_move2 = "Shadow Strike"
-    P1_character1_move3 = "Blade Spin"
-    P1_character1_move4 = "Dark Charge"
+def character1_check():
+#checking what character is and what moves should be
+    if P1_character1 == ("Omega"):
+        P1_character1_1move1 = "Dark Slash"
+        P1_character1_move2 = "Shadow Strike"
+        P1_character1_move3 = "Blade Spin"
+        P1_character1_move4 = "Dark Charge"
+    else:
+        character1_check
+character1_check
 print(P1_character1 + "\'s moves are," + P1_character1_move1 + ", " + P1_character1_move2 +", " + P1_character1_move3 + ", " + P1_character1_move4 + ". ")
