17f5bd2180a1a931ad4166d736cd660432b83b09
yuHyuHyuHmaster <piersontaylor@gmail.com>
2018-02-19 16:14:16 -0600

    bug fixes + more
    simple syntax fixes
    adds a check for Omega and then makes the moves = Omega's moves

--- OmegaProto.py
+++ OmegaProto.py
@@ -2,8 +2,9 @@ allCharacters = ["Omega"]
 print("current characters are:")
 print (allCharacters)
 P1_character1 = input("whats is your first character going to be?")
-P1_character1_1move1 = "Dark Slash"
-P1_character1_move2 = "Shadow Strike"
-P1_character1_move3 = "Blade Spin"
-P1_character1_move4 = "Dark Charge"
-print(P1_character1"\"s moves are," + P1_character1_move1 + ", " + P1_character1_move2 +", " + P1_character1_move3 + ", " + P1_character1_move4 + ". ")
+if P1_character1 = ("Omega"):
+    P1_character1_1move1 = "Dark Slash"
+    P1_character1_move2 = "Shadow Strike"
+    P1_character1_move3 = "Blade Spin"
+    P1_character1_move4 = "Dark Charge"
+print(P1_character1 + "\'s moves are," + P1_character1_move1 + ", " + P1_character1_move2 +", " + P1_character1_move3 + ", " + P1_character1_move4 + ". ")
