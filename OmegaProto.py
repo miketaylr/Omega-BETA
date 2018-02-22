d40e98848dffd415cba14821248b51f52dace334
yuHyuHyuHmaster <piersontaylor@gmail.com>
2018-02-22 17:45:56 -0600

    YEE
    fam

--- OmegaProto.py
+++ OmegaProto.py
@@ -1,16 +1,15 @@
-allCharacters = ['Omega, Nebula']
+allCharacters = 'Omega'
 print("current characters are:")
 print allCharacters
-P1_character1 = input("whats is your first character going to be?")
+P1_character1 = input("what's your first character going to be?")
 def character1_check():
-#checking what character is and what moves should be
-    if P1_character1 == ("Omega"):
-        P1_character1_1move1 = "Dark Slash"
+    #checking what character is and what moves should be
+    if P1_character1 == ("Omega".lower()):
+        P1_character1_move1 = "Dark Slash"
         P1_character1_move2 = "Shadow Strike"
         P1_character1_move3 = "Blade Spin"
-        P1_character1_move4 = "Dark Charge"
+        P1_character1_move4 = "Dark Charge" 
     else:
         character1_check
-character1_check
+character1_check ()
 print(P1_character1 + "\'s moves are," + P1_character1_move1 + ", " + P1_character1_move2 +", " + P1_character1_move3 + ", " + P1_character1_move4 + ". ")
-print('yuh')
